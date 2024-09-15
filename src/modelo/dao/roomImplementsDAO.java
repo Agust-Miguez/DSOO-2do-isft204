33ae7bbbbd5c3d49b53b3035a51aff16a443cc57
Agustin Miguez <143726897+Agust-Miguez@users.noreply.github.com>
2024-09-14 22:29:27 -0300

    Dao implements in rooms

--- src/modelo/dao/roomImplementsDAO.java
+++ src/modelo/dao/roomImplementsDAO.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import modelo.entidades.room;
 import Exceptions.NotfoundException;
+import static java.awt.AWTEventMulticaster.remove;
 
 public class roomImplementsDAO implements DAO <room> {
     
@@ -17,9 +18,12 @@ public class roomImplementsDAO implements DAO <room> {
     public void save(room data) throws DuplicateEntryException  {
      for ( room s : this.rooms){
          if (s.getId()== data.getId()){
-             throw new DuplicateEntryException ("Room whit " + data.getId() +" already exists");
+             throw new DuplicateEntryException ("Room whit " + data.getId() +" already exists if your create new Room press 1 or 2 for update");
+             /*if (Dependiendo el requeriemiento por consola o interfaz grafica llamo a la funcion update o creo nuevo elemento)*/
+                 
          }
      }
+     data.setId(this.rooms.size()+1);
      this.rooms.add(data);
     } 
     
@@ -35,16 +39,31 @@ public class roomImplementsDAO implements DAO <room> {
 
     @Override
     public List<room> listall() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.rooms;
     }
 
     @Override
     public void update(room data) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         for ( room s : this.rooms){
+         if (s.getId()== data.getId()){  
+         }
+     }
     }
 
     @Override
     public void delete(room data) {
+              for ( room s : this.rooms){
+         if (s.getId()== data.getId()){
+            rooms.remove(data.getId());
+         }    
+         }
+    }
+
+    private void getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void remove(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
